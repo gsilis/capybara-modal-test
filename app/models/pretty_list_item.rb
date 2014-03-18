42f7be180f599429c8d8434d5a63b0ab47a01f79
Girts Silis <girts.silis@gmail.com>
2014-03-18 15:59:59 -0400

    Default scope on PrettyListItem model

--- app/models/pretty_list_item.rb
+++ app/models/pretty_list_item.rb
@@ -1,4 +1,6 @@
 class PrettyListItem < ActiveRecord::Base
   validates_presence_of     :title
   validates_numericality_of :sort_order, greater_than: 0
+
+  default_scope             lambda{ order('sort_order asc') }
 end
