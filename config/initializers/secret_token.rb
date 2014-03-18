e0402f0b5b47d2e239516fcd649b30a8be983471
Girts Silis <girts.silis@gmail.com>
2014-03-18 10:19:05 -0400

    Commit initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapybaraModalTest::Application.config.secret_key_base = 'd3f682fe083f80f44610296dba30e0ce3bf9ce14a8db2e8a6f6782791ceb7c6a8d8d2760b61c9ab4f69899b52d5a525f776526e330b41739af3d6d3914c24070'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CapybaraModalTest::Application.config.session_store :cookie_store, key: '_capybara-modal-test_session'
