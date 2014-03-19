685016b6508dac1304c1c94957ed98e381bfc872
Girts Silis <girts.silis@gmail.com>
2014-03-18 21:42:49 -0400

    PrettyListItem change attribute to name

--- app/models/pretty_list_item.rb
+++ app/models/pretty_list_item.rb
@@ -1,5 +1,5 @@
 class PrettyListItem < ActiveRecord::Base
-  validates_presence_of     :title
+  validates_presence_of     :name
   validates_numericality_of :sort_order, greater_than: 0
 
   default_scope             lambda{ order('sort_order asc') }
