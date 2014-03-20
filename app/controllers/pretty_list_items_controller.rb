a7f1bfd1e99eb0e5e5461be6545a0fbb55645dc8
Girts Silis <girts.silis@gmail.com>
2014-03-20 14:14:02 -0400

    Destroy method for pretty list item controller

--- app/controllers/pretty_list_items_controller.rb
+++ app/controllers/pretty_list_items_controller.rb
@@ -1,11 +1,28 @@
 class PrettyListItemsController < ApplicationController
   before_action :fetch_pretty_list_items
+  before_action :fetch_pretty_list_item, only: [:destroy]
 
   def index
+  end
+
+  def destroy
+    if @list_item.destroy
+      flash.now[:notice] = t('activerecord.models.pretty_list_item.flash.destroy.notice', name: @list_item.name)
+    else
+      flash.now[:error] = t('activerecord.models.pretty_list_item.flash.destroy.error', name: @list_item.name)
+    end
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   private
     def fetch_pretty_list_items
       @list_items = PrettyListItem.all
+    end
+
+    def fetch_pretty_list_item
+      @list_item = PrettyListItem.find params[:id]
     end
 end
