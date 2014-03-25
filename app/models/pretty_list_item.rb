c140fd812338e71def120d8c2aa53e60f06886f4
Girts Silis <girts.silis@gmail.com>
2014-03-24 23:08:46 -0400

    Make PrettyListItem validate uniqueness of sort order

--- app/models/pretty_list_item.rb
+++ app/models/pretty_list_item.rb
@@ -1,6 +1,7 @@
 class PrettyListItem < ActiveRecord::Base
   validates_presence_of     :name
   validates_numericality_of :sort_order, greater_than: 0
+  validates_uniqueness_of   :sort_order
 
   default_scope             lambda{ order('sort_order asc') }
 end
