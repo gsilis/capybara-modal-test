89ef4294ed947d73bd1345d372a6ee4d96dc1943
Girts Silis <girts.silis@gmail.com>
2014-03-24 23:11:20 -0400

    Initialization hook on pretty list item

--- app/models/pretty_list_item.rb
+++ app/models/pretty_list_item.rb
@@ -4,4 +4,11 @@ class PrettyListItem < ActiveRecord::Base
   validates_uniqueness_of   :sort_order
 
   default_scope             lambda{ order('sort_order asc') }
+
+  after_initialize          :initialize_with_defaults, if: :new_record?
+
+  private
+    def initialize_with_defaults
+      self.sort_order = (self.class.maximum(:sort_order) || 0) + 1
+    end
 end
