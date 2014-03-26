ec3f9d0bb81fcc257646c0a40d45c2188d4f0f67
Girts Silis <girts.silis@gmail.com>
2014-03-25 23:33:18 -0400

    Nitpicky formatting of index spec file
    Seems to look better! But we'll see if it holds up.

--- spec/features/pretty_list_items/index_spec.rb
+++ spec/features/pretty_list_items/index_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Pretty list items index' do
 
   let!(:pretty_list_items) { Fabricate.times 4, :pretty_list_item }
-  let!(:create_params) { { name: Faker::Name.name, description: Faker::Lorem.paragraph } }
+  let!(:valid_params) { { name: Faker::Name.name, description: Faker::Lorem.paragraph } }
 
   scenario 'it displays the pretty list items properly' do
     visit '/'
@@ -20,26 +20,25 @@ feature 'Pretty list items index' do
   end
 
   scenario 'it removes the item from the page', :js => true do
-    visit '/'
-
     first_list_item = pretty_list_items.first
+
+    visit '/'
     click_link 'Remove', href: pretty_list_item_path(first_list_item)
 
     expect(page).to_not have_css("#pretty_list_item_#{first_list_item.id}")
-    expect(page).to have_text("'#{first_list_item.name}' was removed from the list.")
+    expect(page).to     have_text("'#{first_list_item.name}' was removed from the list.")
   end
 
   scenario 'it can add new list items', js: true do
     visit '/'
 
-    click_link 'Add', href: new_pretty_list_item_path
+    click_link    'Add',          href: new_pretty_list_item_path
+    fill_in       'Name',         with: valid_params[:name]
+    fill_in       'Description',  with: valid_params[:description]
+    click_button  'Save'
 
-    fill_in 'Name',         with: create_params[:name]
-    fill_in 'Description',  with: create_params[:description]
-    click_button 'Save'
-
-    expect(page).to have_xpath("//div[@class=\"pretty-list\"]/ul/li[last()]/span", text: create_params[:name])
-    expect(page).to have_text("'#{create_params[:name]}' was added to the list.")
+    expect(page).to     have_xpath("//div[@class=\"pretty-list\"]/ul/li[last()]/span", text: valid_params[:name])
+    expect(page).to     have_text("'#{valid_params[:name]}' was added to the list.")
     expect(page).to_not have_selector('.modal-backdrop')
     expect(page).to_not have_selector('.modal.in')
   end
@@ -50,9 +49,9 @@ feature 'Pretty list items index' do
     click_link 'Add', href: new_pretty_list_item_path
     click_button 'Save'
 
-    expect(page).to have_text('Item Name can\'t be blank')
+    expect(page).to     have_text('Item Name can\'t be blank')
+    expect(page).to     have_selector('.modal-backdrop', count: 1)
     expect(page).to_not have_text('Sort order is not a number')
-    expect(page).to have_selector('.modal-backdrop', count: 1)
   end
 
 end
