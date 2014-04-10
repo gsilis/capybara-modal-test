252d3d32983f9d7c43c7001d5bb7a2792f479716
Girts Silis <girts.silis@gmail.com>
2014-04-10 01:02:57 -0400

    Flash JS file has helper methods for wrapping strings as messages

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -11,3 +11,39 @@ function displayFlashMessage(html_to_insert) {
     $('.alert').remove();
     $('h1').after(html_to_insert);
 };
+
+
+/**
+ * Wraps the text passed with flash message HTML tags
+ * @param  {string} message_type - The type of message to (info|danger)
+ * @param  {string} message_text - The text to display
+ * @return {string}              - HTML that can be inserted via displayFlashMessage
+ */
+function wrapWithFlashMessageHtml(message_type, message_text) {
+
+    var classNames = ['alert', 'alert-dismissable', 'alert-' + message_type];
+
+    return '<div class="' + classNames.join(' ') + '">' +
+        message_text +
+        '</div>';
+};
+
+/**
+ * Wraps the text passed within a flash message wrapper of type info
+ * @param  {string} message_text - The text to wrap
+ * @return {string}              - HTML that can be inserted via displayFlashMessage
+ */
+function wrapWithFlashMessageHtmlInfo(message_text) {
+
+    return wrapWithFlashMessageHtml('info', message_text);
+};
+
+/**
+ * Wraps the text passed within a flash message wrapper of type danger
+ * @param  {string} message_text - The text to wrap
+ * @return {string}              - HTML that can be inserted via displayFlashMessage
+ */
+function wrapWithFlashMessageHtmlDanger(message_text) {
+
+    return wrapWithFlashMessageHtml('danger', message_text);
+};
