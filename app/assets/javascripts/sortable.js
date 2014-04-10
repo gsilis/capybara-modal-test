cb4e878b6f2765680b75541ff1d556d8a8a44c80
Girts Silis <girts.silis@gmail.com>
2014-04-09 23:06:19 -0400

    More skeleton JS functions for list sortability

--- app/assets/javascripts/sortable.js
+++ app/assets/javascripts/sortable.js
@@ -1,4 +1,34 @@
+//
 // Functions to support list sorting
+//
+
+/**
+ * Initiates a request to save the reordered list items
+ * @param  {array} ids - The list of IDs in correct order
+ */
 function update_list_order(ids) {
 
-}
+
+};
+
+/**
+ * Handler for the sort order ajax response
+ * Check out {@link https://api.jquery.com/jQuery.ajax}
+ * @param  {jqXHR} jqXHR        - An object provided by jQuery
+ * @param  {string} testStatus  - The status reported from jQuery
+ */
+function onUpdateListOrderResponse(jqXHR, testStatus) {
+
+
+};
+
+/**
+ * Handler for the jquery ui stop event
+ * Check out {@link http://api.jqueryui.com/sortable}
+ * @param  {event}  event   - The event object supplied by jQuery UI
+ * @param  {object} ui      - An object supplied by jQuery UI
+ */
+function onUpdateListOrder(event, ui) {
+
+
+};
